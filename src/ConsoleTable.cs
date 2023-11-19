43ee5f592c700dde782eca45a763714762015028
Luc Allaire <wolf-361@hotmail.ca>
2023-11-18 22:06:58 -0500

    fix the rows not updating if adding larger content later and the headers

--- src/ConsoleTable.cs
+++ src/ConsoleTable.cs
@@ -28,15 +28,6 @@ public class ConsoleTable
 
     // ----- Private Methods -----
 
-    /// <summary>
-    /// Get the width of the column
-    /// </summary>
-    /// <param name="columnIndex">The index of the column</param>
-    /// <returns>The width of the column</returns>
-    private int GetColumnWidth(int columnIndex)
-    {
-        return _columnWidths[columnIndex];
-    }
 
     private static string GetLine(int columnWidth)
     {
@@ -54,7 +45,7 @@ public class ConsoleTable
         sb.Append(TopLeft);
         for (var i = 0; i < _columnCount; i++)
         {
-            sb.Append(GetLine(GetColumnWidth(i)));
+            sb.Append(GetLine(_columnWidths[i]));
             sb.Append(TopCross);
         }
         sb.Remove(sb.Length - 1, 1);
@@ -68,7 +59,7 @@ public class ConsoleTable
         sb.Append(BottomLeft);
         for (var i = 0; i < _columnCount; i++)
         {
-            sb.Append(GetLine(GetColumnWidth(i)));
+            sb.Append(GetLine(_columnWidths[i]));
             sb.Append(BottomCross);
         }
         sb.Remove(sb.Length - 1, 1);
@@ -82,7 +73,7 @@ public class ConsoleTable
         sb.Append(LeftCross);
         for (var i = 0; i < _columnCount; i++)
         {
-            sb.Append(GetLine(GetColumnWidth(i)));
+            sb.Append(GetLine(_columnWidths[i]));
             sb.Append(Cross);
         }
         sb.Remove(sb.Length - 1, 1);
@@ -93,14 +84,29 @@ public class ConsoleTable
     private string GetRowString(string[] row)
     {
         StringBuilder sb = new();
+
+        // Ajust the length of the rows if needed
+        if (row.Length < _columnCount)
+        {
+            var newRow = new string[_columnCount];
+            for (var i = 0; i < _columnCount; i++)
+            {
+                if (row.Length > i)
+                    newRow[i] = row[i];
+                else
+                    newRow[i] = Empty;
+            }
+            row = newRow;
+        }
+
         for (var i = 0; i < _columnCount; i++)
         {
             sb.Append(VerticalLine);
             sb.Append(Space);
 
             // Center the text in the column
-            var padding = (GetColumnWidth(i) - row[i].Length) / 2;
-            sb.Append(row[i].PadLeft(padding + row[i].Length, Space).PadRight(GetColumnWidth(i), Space));
+            var padding = (_columnWidths[i] - row[i].Length) / 2;
+            sb.Append(row[i].PadLeft(padding + row[i].Length, Space).PadRight(_columnWidths[i], Space));
             sb.Append(Space);
         }
         sb.Append(VerticalLine);
@@ -109,7 +115,31 @@ public class ConsoleTable
 
     private void AddRow(string[] row)
     {
-        // Add the row to the table (ajust the length of the row if needed)
+        // Update the column count (fist cause the value is used later)
+        if (row.Length > _columnCount)
+            _columnCount = row.Length;
+
+        // Update the column widths
+        for (var i = 0; i < _columnCount; i++)
+        {
+            var width = row[i].Length;
+
+            // Check if there is currently a value at this index
+            try
+            {
+                if (width > _columnWidths[i])
+                {
+                    _columnWidths[i] = width;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                _columnWidths.Add(width);
+            }
+
+        }
+
+        // Add the row to the table (ajust the length of the rows if needed)
         if (row.Length < _columnCount)
         {
             var newRow = new string[_columnCount];
@@ -123,28 +153,21 @@ public class ConsoleTable
             row = newRow;
         }
         _rows.Add(row);
-
-        // Update the column widths
-        for (var i = 0; i < _columnCount; i++)
-        {
-            var width = row[i].Length;
-            if (width > GetColumnWidth(i))
-            {
-                _columnWidths[i] = width;
-            }
-        }
-
-        // Update the column count
-        if (row.Length > _columnCount)
-            _columnCount = row.Length;
     }
 
     // ----- Public Methods -----
 
-    public void SetTitles(params string[] columnNames)
+    public void AddColumn(string columnName)
+    {
+        _columnNames.Add(columnName);
+        _columnCount++;
+        _columnWidths.Add(columnName.Length);
+    }
+
+    public void AddColumns(params string[] columnNames)
     {
         _columnNames.AddRange(columnNames);
-        _columnCount = columnNames.Length;
+        _columnCount += columnNames.Length;
         _columnWidths.AddRange(columnNames.Select(name => name.Length));
     }
 
@@ -172,10 +195,17 @@ public class ConsoleTable
     {
         StringBuilder sb = new();
 
-        // Sandwitch the table colomn names between the top and middle border
-        sb.AppendLine(GetTopBorder());
-        sb.AppendLine(GetRowString(_columnNames.ToArray()));
-        sb.AppendLine(GetMiddleBorder());
+        // Sandwich the table colomn names between the top and middle border (if there are any)
+        if (_columnNames.Count > 0)
+        {
+            sb.AppendLine(GetTopBorder());
+            sb.AppendLine(GetRowString(_columnNames.ToArray()));
+            sb.AppendLine(GetMiddleBorder());
+        }
+        else
+        {
+            sb.AppendLine(GetTopBorder());
+        }
 
         // Add the rows
         foreach (var row in _rows)
